a109338cd2d0082a0711614e8f2acae68a914d0e
basicjohn <edmondsonj@gmail.com>
2021-07-23 14:53:04 -0700

    WIP: Google OAUTH on .Net

--- Server/Program.cs
+++ Server/Program.cs
@@ -13,6 +13,19 @@ namespace Server
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                WriteTo.Console()
+                .CreateLogger();
+
+            Helpers.SimpleLogger.Log("Starting Service");
+
+            string json = File.ReadAllText("appsettings.json");
+            JObject o = JObject.Parse(json);
+            AppSettings.appSettings = JsonConvert.DeserializeObject<AppSettings>(o["AppSettings"].ToString());
+
+            //Helpers.SimpleLogger
+
             CreateHostBuilder(args).Build().Run();
         }
 
