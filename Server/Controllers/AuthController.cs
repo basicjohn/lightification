3e6dd6c701bb8f5925f05ebab643da875437f6ef
basicjohn <edmondsonj@gmail.com>
2021-07-23 16:36:35 -0700

    create the google endpoint

---
+++ Server/Controllers/AuthController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Server.Helpers;
+using Server.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Google.Apis.Auth;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Server.Controllers
+{
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class WeatherForecastController : ControllerBase
+//     {
+//         private static readonly string[] Summaries = new[]
+//         {
+//             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//         };
+
+//         private readonly ILogger<WeatherForecastController> _logger;
+
+//         public WeatherForecastController(ILogger<WeatherForecastController> logger)
+//         {
+//             _logger = logger;
+//         }
+
+//         [HttpGet]
+//         public IEnumerable<WeatherForecast> Get()
+//         {
+//             var rng = new Random();
+//             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+//             {
+//                 Date = DateTime.Now.AddDays(index),
+//                 TemperatureC = rng.Next(-20, 55),
+//                 Summary = Summaries[rng.Next(Summaries.Length)]
+//             })
+//             .ToArray();
+//         }
+//     }
+    [AllowAnonymous]
+    [HttpPost("google")]
+    public async Task<IActionResult> Google([FromBody]UserView userView)
+    {
+        try
+        {
+            //SimpleLogger.Log("userView = " + userView.tokenId);
+            var payload = GoogleJsonWebSignature.ValidateAsync(userView.tokenId, new GoogleJsonWebSignature.ValidationSettings()).Result;
+            var user = await _authService.Authenticate(payload);
+            SimpleLogger.Log(payload.ExpirationTimeSeconds.ToString());
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, Security.Encrypt(AppSettings.appSettings.JwtEmailEncryption,user.email)),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AppSettings.appSettings.JwtSecret));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(String.Empty,
+                String.Empty,
+                claims,
+                expires: DateTime.Now.AddSeconds(55*60),
+                signingCredentials: creds);
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+        catch (Exception ex)
+        {
+            Helpers.SimpleLogger.Log(ex);
+            BadRequest(ex.Message);
+        }
+        return BadRequest();
+    }
+}
