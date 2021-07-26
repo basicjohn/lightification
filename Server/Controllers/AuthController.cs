e6d40af6bae6758a023e84eb4418f2af7041d776
basicjohn <edmondsonj@gmail.com>
2021-07-26 16:20:25 -0700

    add boilerplate & models

--- Server/Controllers/AuthController.cs
+++ Server/Controllers/AuthController.cs
@@ -16,70 +16,62 @@ using System.Text;
 
 namespace Server.Controllers
 {
-//     [ApiController]
-//     [Route("[controller]")]
-//     public class WeatherForecastController : ControllerBase
-//     {
-//         private static readonly string[] Summaries = new[]
-//         {
-//             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//         };
-
-//         private readonly ILogger<WeatherForecastController> _logger;
-
-//         public WeatherForecastController(ILogger<WeatherForecastController> logger)
-//         {
-//             _logger = logger;
-//         }
-
-//         [HttpGet]
-//         public IEnumerable<WeatherForecast> Get()
-//         {
-//             var rng = new Random();
-//             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-//             {
-//                 Date = DateTime.Now.AddDays(index),
-//                 TemperatureC = rng.Next(-20, 55),
-//                 Summary = Summaries[rng.Next(Summaries.Length)]
-//             })
-//             .ToArray();
-//         }
-//     }
-    [AllowAnonymous]
-    [HttpPost("google")]
-    public async Task<IActionResult> Google([FromBody]UserView userView)
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
     {
-        try
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
         {
-            //SimpleLogger.Log("userView = " + userView.tokenId);
-            var payload = GoogleJsonWebSignature.ValidateAsync(userView.tokenId, new GoogleJsonWebSignature.ValidationSettings()).Result;
-            var user = await _authService.Authenticate(payload);
-            SimpleLogger.Log(payload.ExpirationTimeSeconds.ToString());
-
-            var claims = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, Security.Encrypt(AppSettings.appSettings.JwtEmailEncryption,user.email)),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            };
-
-            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AppSettings.appSettings.JwtSecret));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(String.Empty,
-                String.Empty,
-                claims,
-                expires: DateTime.Now.AddSeconds(55*60),
-                signingCredentials: creds);
-            return Ok(new
-            {
-                token = new JwtSecurityTokenHandler().WriteToken(token)
-            });
+            this._authService = authService;
         }
-        catch (Exception ex)
+
+        [AllowAnonymous]
+        [HttpPost("google")]
+        public async Task<IActionResult> Google([FromBody]UserView userView)
         {
-            Helpers.SimpleLogger.Log(ex);
-            BadRequest(ex.Message);
+            try
+            {
+                SimpleLogger.Log("userView = " + userView.tokenId);
+                var payload = GoogleJsonWebSignature.ValidateAsync(userView.tokenId, new GoogleJsonWebSignature.ValidationSettings()).Result;
+                var user = await _authService.Authenticate(payload);
+                SimpleLogger.Log(payload.ExpirationTimeSeconds.ToString());
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, Security.Encrypt(AppSettings.appSettings.JwtEmailEncryption,user.email)),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AppSettings.appSettings.JwtSecret));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(String.Empty,
+                  String.Empty,
+                  claims,
+                  expires: DateTime.Now.AddSeconds(55*60),
+                  signingCredentials: creds);
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            catch (Exception ex)
+            {
+                Helpers.SimpleLogger.Log(ex);
+                BadRequest(ex.Message);
+            }
+            return BadRequest();
         }
-        return BadRequest();
     }
 }
+
+/*SimpleLogger.Log(payload.Name);
+                SimpleLogger.Log(payload.Email);
+                SimpleLogger.Log(payload.Subject);
+                SimpleLogger.Log(payload.Issuer);
+                SimpleLogger.Log(payload.ExpirationTimeSeconds.ToString());
+
+                SimpleLogger.Log(DateUtil.FromUnixTime((long)payload.ExpirationTimeSeconds).ToString());
+                SimpleLogger.Log(payload.JwtId);*/
--- Server/Services/IAuthService.cs
+++ Server/Services/IAuthService.cs
@@ -6,28 +6,59 @@ using System.Linq;
 
 namespace Server.Services
 {
-
-  public async Task<User> Authenticate(Google.Apis.Auth.GoogleJsonWebSignature.Payload payload)
-  {
-    await Task.Delay(1);
-    return this.FindUserOrAdd(payload);
-  }
-
-  private User FindUserOrADd(Google.Apis.Auth.GoogleJsonWebSignature.Payload payload)
-  {
-    var u = _users.Where(x => x.email == payload.Email).FirstOrDefault();
-    if (u == null)
+    public interface IAuthService
     {
-      u = new User()
-      {
-        id = Guid.NewGuid(),
-        name = payload.Name,
-        email = payload.Email,
-        oauthSubject = payload.Subject,
-        oauthIssuer = payload.Issuer
-      };
-      _users.Add(u);
-  }
-  this.PrintUsers();
-  return u;
+        Task<User> Authenticate(Google.Apis.Auth.GoogleJsonWebSignature.Payload payload);
+    }
+
+    public class AuthService : IAuthService
+    {
+        public AuthService()
+        {
+            this.Refresh();
+        }
+        private static IList<User> _users = new List<User>();
+        public async Task<User> Authenticate(Google.Apis.Auth.GoogleJsonWebSignature.Payload payload)
+        {
+            await Task.Delay(1);
+            return this.FindUserOrAdd(payload);
+        }
+
+        private User FindUserOrAdd(Google.Apis.Auth.GoogleJsonWebSignature.Payload payload)
+        {
+            var u = _users.Where(x => x.email == payload.Email).FirstOrDefault();
+            if (u == null)
+            {
+                u = new User()
+                {
+                    id = Guid.NewGuid(),
+                    name = payload.Name,
+                    email = payload.Email,
+                    oauthSubject = payload.Subject,
+                    oauthIssuer = payload.Issuer
+                };
+                _users.Add(u);
+            }
+            this.PrintUsers();
+            return u;
+        }
+
+        private void PrintUsers()
+        {
+            string s = String.Empty;
+            foreach (var u in _users) s += "\n[" + u.email + "]";
+            Helpers.SimpleLogger.Log(s);
+        }
+
+        private void Refresh()
+        {
+            if (_users.Count == 0)
+            {
+                _users.Add(new User() { id = Guid.NewGuid(), name = "Test Person1", email = "testperson1@gmail.com" });
+                _users.Add(new User() { id = Guid.NewGuid(), name = "Test Person2", email = "testperson2@gmail.com" });
+                _users.Add(new User() { id = Guid.NewGuid(), name = "Test Person3", email = "testperson3@gmail.com" });
+                this.PrintUsers();
+            }
+        }
+    }
 }
